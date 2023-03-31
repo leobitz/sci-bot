8a7d2a0fb741d1da432f2d83b7626012ebffbbd6
Leo Bitz <amanyamy@gmail.com>
2023-04-01 01:39:59 +0300

    class based chatbot

--- engine.py
+++ engine.py
@@ -5,83 +5,160 @@ import numpy as np
 from transformers import pipeline
 from summarizer.sbert import SBertSummarizer
 
+from top2vec import Top2Vec
+# class DocHierarchicalSearch:
+
+#     def __init__(self, encoder) -> None:
+#         self.encoder = encoder
+
+#     def build_tree(self, doc2vec):
+
+
+
+class SlidingWindowDoc2Vec:
+
+    def __init__(self, encoder, window_size=256, stride=128) -> None:
+        self.window_size = 256
+        self.stride = 128
+        self.encoder = encoder
+    
+    def encode(self, text):
+        words = text.split()
+
+        if len(words) <= self.window_size:
+            return self.encoder([text])
+        
+        windows = []
+        for i in range(0, len(words) - self.stride, self.stride):
+            sub_text = " ".join(words[i:i + self.window_size])
+            windows.append(sub_text)
+
+        return self.encoder(windows).mean(axis=0)
+
 
 class ChatBotEngine:
 
-    def __init__(self) -> None:
+    def __init__(self, 
+                document_file_path,
+                doc2vec = 'summarize', 
+                 explain = False, 
+                 rephrase_explain = False, 
+                 exp_sentences = 3, 
+                 confidence_threshold = 0.5, 
+                 max_num_samples = 10,
+                 window_size: int = 128, 
+                 stride: int = 128) -> None:
+        self.explain = explain
+        self.rephrase_explain = rephrase_explain
+        self.exp_sentences = exp_sentences
+        self.confidence_threshold = confidence_threshold
+        self.window_size = window_size
+        self.stride = stride
+        self.max_num_samples = max_num_samples
+
         self.SUMMARIZER_MODEL = SBertSummarizer('paraphrase-MiniLM-L6-v2')
         self.SENTENCE_ENCODER = SentenceTransformer('all-MiniLM-L6-v2')
         self.Q_ANSWERER = pipeline("question-answering", model='distilbert-base-cased-distilled-squad')
         self.REPHRASER = self.SUMMARIZER_MODEL
-        self.CORPUS_DB, self.CORPUS_EMB = self.prepare_doc_encoding('data/val.txt', 10, 128)
-        
+        self.topic_model = None
 
+        if doc2vec == 'summarize':
+            self.doc2vec_encode = self.SENTENCE_ENCODER.encode
+        elif doc2vec == 'ave_window':
+            encoder = SlidingWindowDoc2Vec(self.SENTENCE_ENCODER.encode, self.window_size, self.stride)
+            self.doc2vec_encode = encoder.encode
+        else:
+            raise Exception("doc2vec strategy not specified")
+
+        self.CORPUS_DB, self.CORPUS_EMB = self.prepare_doc_encoding(document_file_path, self.max_num_samples)
 
     def bert_summarize_text(self, texts: List[str], max_word_length: int) -> str:
         result = self.SUMMARIZER_MODEL(texts, min_length=max_word_length)
         return result
 
-    def prepare_doc_encoding(self, file_name: str, num_samples: int, window_size: int = 128) -> List:
-        processed = []
+    def prepare_top_vec(self, file_name, num_samples: int):
         file = open(file_name, encoding='utf-8', mode='r')
         raw_articles = []
+        
         for i in range(num_samples):
             art = json.loads(file.readline())
             passage = " ".join(art['article_text'])
             raw_articles.append(passage)
 
-            if len(passage.split()) > window_size:
-                passage = self.bert_summarize_text(passage, max_word_length=window_size)
+        file.close()
+        self.topic_model = Top2Vec(raw_articles, embedding_model="all-MiniLM-L6-v2")
+
+    def prepare_doc_encoding(self, file_name: str, num_samples: int) -> List:
+        file = open(file_name, encoding='utf-8', mode='r')
+        raw_articles = []
+        doc_vec = []
+        
+        for i in range(num_samples):
+            art = json.loads(file.readline())
+            passage = " ".join(art['article_text'])
+            raw_articles.append(passage)
+
+            if len(passage.split()) > self.window_size:
+                passage = self.bert_summarize_text(passage, max_word_length=self.window_size)
             
-            processed.append(passage)
+            enc = self.doc2vec_encode(passage)
+            doc_vec.append(enc)
 
         file.close()
         
-        doc_vec = self.SENTENCE_ENCODER.encode(processed)
+        doc_vec = np.stack(doc_vec)
 
         return raw_articles, doc_vec
 
 
     def search(self, question: str, corpus_emb: np.ndarray,  top_k: int = 5) -> str:
-        question_emb = self.SENTENCE_ENCODER.encode(question)
-        hits = util.semantic_search(question_emb, corpus_emb, top_k=top_k)
+        if self.topic_model:
+            hits = self.topic_model.query_documents(question, 1)
+        else:
+            question_emb = self.SENTENCE_ENCODER.encode(question)
+            hits = util.semantic_search(question_emb, corpus_emb, top_k=top_k)
         return hits
 
     def answer(self, question: str, context: str) -> str:
         result = self.Q_ANSWERER(question=question, context=context)
         return result
 
-
-    def rephrase_explanation(self, final_context, exp_sentences=3):
-        new_explanation = self.REPHRASER(final_context, num_sentences=exp_sentences)
+    def rephrase_explanation(self, final_context):
+        new_explanation = self.REPHRASER(final_context, num_sentences=self.exp_sentences)
         return new_explanation
 
-    def query(self, question: str, explain = False, rephrase_explain = False, exp_sentences = 3, confidence_threshold = 0.5):
+    def query(self, question: str):
+
+        if question == None or question.strip() == '' or len(question.split()) <= 2:
+            return "Please give me a question with at least 3 words"
 
         hits = self.search(question, self.CORPUS_EMB)
 
-        if hits[0][0]['score'] >= confidence_threshold:
+        # if a document that is similar to the question intent is found
+        if hits[0][0]['score'] >= self.confidence_threshold: 
+
             context = self.CORPUS_DB[hits[0][0]['corpus_id']]
             result = self.answer(question, context)
             
             exp = None
 
             score, ans = result['score'], result['answer']
-            if explain:
+            if self.explain:
                 exp = context
-                if rephrase_explain:
-                    exp = self.rephrase_explanation(exp, exp_sentences= exp_sentences)
+                if self.rephrase_explain:
+                    exp = self.rephrase_explanation(exp)
                 ans = f"{ans} \n Explanation: {exp}"
             
             print(score)
-            if score > confidence_threshold:
+            if score > self.confidence_threshold:
                 return ans
         
         return "Sorry, I don't know" 
 
-engine = ChatBotEngine()
+if __name__ == "__main__":
+    engine = ChatBotEngine(document_file_path='data/val.txt', max_num_samples=100)
 
-question = "what is neurologists?"
-ans = engine.query(question, True, True, exp_sentences=2, confidence_threshold=0.5)
+    question = "who decides whether stroke status is correct?"
+    ans = engine.query(question)
 
-print(ans)
+    print(ans)
--- main.py
+++ main.py
@@ -5,14 +5,16 @@ from ontology_dc8f06af066e4a7880a5938933236037.simple_text import SimpleText
 from openfabric_pysdk.context import OpenfabricExecutionRay
 from openfabric_pysdk.loader import ConfigClass
 from time import time
+from engine import ChatBotEngine
 
-
+chatbot_engine: ChatBotEngine = None
 ############################################################
 # Callback function called on update config
 ############################################################
 def config(configuration: ConfigClass):
-    # TODO Add code here
-    pass
+    global chatbot_engine
+    chatbot_engine = ChatBotEngine()
+    
 
 
 ############################################################
@@ -22,7 +24,7 @@ def execute(request: SimpleText, ray: OpenfabricExecutionRay) -> SimpleText:
     output = []
     for text in request.text:
         # TODO Add code here
-        response = ''
+        response = chatbot_engine.query(text)
         output.append(response)
 
     return SimpleText(dict(text=output))
